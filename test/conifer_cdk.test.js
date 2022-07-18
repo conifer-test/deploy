49de35d8289a22178dc388090ec35b59795622b0
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-07-18 14:39:03 -0700

    first commit

---
+++ test/conifer_cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const ConiferCdk = require('../lib/conifer_cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/conifer_cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new ConiferCdk.ConiferCdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
